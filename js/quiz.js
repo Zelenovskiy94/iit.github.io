60c3052da66611c30f86a7bbc3b533b5461220fc
Max <maksim.zelenovskiy@gmail.com>
2021-08-28 10:06:19 +0300

    update

---
+++ js/quiz.js
@@ -0,0 +1,142 @@
+activeSlide(0)
+nextButton ()
+backButton ()
+
+let objData = {}
+
+function activeSlide (item) {
+	let countItems = jQuery('.quiz_question').length
+	jQuery('.progress').css('width', 100 / countItems * (item+1) + '%' )
+	jQuery('.quiz_question').each(function(index) {
+		jQuery(this).attr('data-item', index)
+		if(item == 0){
+			jQuery('#cancel').css('display', 'block')
+			jQuery('#back').css('display', 'none')
+		} else {
+			jQuery('#cancel').css('display', 'none')
+			jQuery('#back').css('display', 'block')
+		}
+		if(index == item) {
+			jQuery(this).addClass('active')
+			if(jQuery(this).hasClass('valid')) {
+				nextButton (true)
+			} else {
+				nextButton (false)
+			}
+		} else {
+			jQuery(this).removeClass('active')
+		}
+	})
+	if(item == countItems - 1) {
+		jQuery('#next').addClass('finish')
+	}
+	
+	backButton ()
+}
+function nextButton (status = false, last) {
+	let currentSlideIndex = +jQuery('.quiz_question.active').attr('data-item')
+	let buttunNext = jQuery('#next')
+	buttunNext.attr('next-item', currentSlideIndex + 1)
+	if(status) {
+		jQuery('#next').removeClass('disabled')
+	} else {
+		jQuery('#next').addClass('disabled')
+	}
+
+}
+function backButton () {
+	let currentSlideIndex = +jQuery('.quiz_question.active').attr('data-item')
+	let buttunBack = jQuery('#back')
+	let prevSlide = currentSlideIndex - 1
+	if(prevSlide < 0) {
+		prevSlide = 0
+	}
+	buttunBack.attr('prev-item', prevSlide)
+	
+}
+
+function isValid (elem) {
+	let inputs = $(elem).find('input[type="text"]')
+	let isValidField = false
+	isValidField = Array.from(inputs).every(function(elem){
+		return $(elem).hasClass('valid')
+		
+	});
+	nextButton(isValidField)
+}
+function validateEmail(email) {
+	let re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+	return re.test(String(email).toLowerCase());
+}
+function validateNameName(str) {
+	return str && isNaN(str) && str.length < 40
+}
+function validPhone(phone) {
+	var re = /^\d[\d\(\)\ -]{4,14}\d$/;
+	return re.test(phone);
+}  
+$('input[type="radio"]').on('click', function(){
+	if($(this).is(':checked')) {
+		nextButton(true)
+		$(this).closest('.quiz_question').addClass('valid')
+	}
+})
+$('#nameSchool, #yourName, #yourEmail, #yourPhone').on('blur', function(){
+	let validClass = () => {
+		$(this).addClass('valid')
+		$(this).removeClass('invalid')
+		$(this).closest('.quiz_question').addClass('valid')
+	}
+	if(this.id == 'nameSchool' && $(this).val() != '') {
+		validClass()
+	} else if(this.id == 'yourEmail' && validateEmail($(this).val()) && $(this).val() != ''){
+		validClass()
+	} else if(this.id == 'yourName' && validateNameName($(this).val()) && $(this).val() != ''){
+		validClass()
+		localStorage.setItem('quiz_name', $(this).val());
+	} else if(this.id == 'yourPhone' && validPhone($(this).val()) && $(this).val() != ''){
+		validClass()
+	} else {
+		$(this).addClass('invalid')
+		$(this).removeClass('valid')
+		$(this).closest('.quiz_question').removeClass('valid')
+	}
+	isValid ($(this).closest('.quiz_question'))
+})
+$('#next').on('click', function(){
+	if( $(this).hasClass('finish') ) {
+		jQuery('.quiz_question input:checked').each(function(index) {
+			let name = $(this).attr('name')
+			objData[name] = $(this).val()
+		})
+		jQuery('.quiz_question input[type="text"]').each(function(index) {
+			let name = $(this).attr('name')
+			objData[name] = $(this).val()
+		})
+		sentResponse({
+			"Do you have access to a computer/laptop/tablet for school purposes?" : objData.structure
+		})
+		return
+	}
+	activeSlide(+$(this).attr('next-item'))
+	document.getElementById('progress').scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+		})
+})
+$('#back').on('click', function(){
+	activeSlide(+$(this).attr('prev-item'))
+	jQuery('#next').removeClass('finish')
+})
+
+function sentResponse(objData) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", 'https://hooks.zapier.com/hooks/catch/10786736/b49g70w/', true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify(objData));
+	// redirectToThanksPage ()
+
+}
+function redirectToThanksPage () {
+	window.location.replace("thanks.html");
+}
--- js/script.js
+++ js/script.js
@@ -16,18 +16,4 @@ $(document).ready(function(){
         $(this).toggleClass('open').next().slideToggle()
     })
 });
-const anchors = document.querySelectorAll('a[href*="#"]')
-
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href').substr(1)
-    
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-}
 
